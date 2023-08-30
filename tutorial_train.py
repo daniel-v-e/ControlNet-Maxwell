bd8942c65b391cb5bac95ef7cccc270019f04cb9
daniel-v-e <daniel@autoscriber.com>
2023-08-30 22:41:23 +0000

    Larger batch size, reintroduced checkpointing

--- tutorial_train.py
+++ tutorial_train.py
@@ -2,7 +2,6 @@ from share import *
 
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
-# Imports
 from tutorial_dataset import MyDataset
 from cldm.logger import ImageLogger
 from cldm.model import create_model, load_state_dict
@@ -10,7 +9,7 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 
 # Configs
 resume_path      = './models/control_sd15_ini.ckpt'
-batch_size       = 4
+batch_size       = 8
 logger_freq      = 300
 learning_rate    = 1e-5
 sd_locked        = True
@@ -25,9 +24,9 @@ model.only_mid_control = only_mid_control
 
 # Checkpoints
 checkpoint_callback = ModelCheckpoint(
-    dirpath        = "checkpoints",
-    filename       = "{epoch:02d}-{step}",
-    every_n_epochs = 1,
+    dirpath             = "models/checkpoints",
+    filename            = "{epoch:02d}-{step}",
+    every_n_train_steps = 3125,
 )
 
 # Misc
@@ -35,6 +34,7 @@ dataset    = MyDataset()
 dataloader = DataLoader(dataset, num_workers=8, batch_size=batch_size, shuffle=True)
 logger     = ImageLogger(batch_frequency=logger_freq)
 trainer    = pl.Trainer(gpus=1, precision=32, callbacks=[logger, checkpoint_callback])
+trainer    = pl.Trainer(gpus=1, precision=32, callbacks=[logger])
 
 # Train!
 trainer.fit(model, dataloader)
